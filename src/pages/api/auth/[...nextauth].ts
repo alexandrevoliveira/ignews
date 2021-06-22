48f4906fc8f6c1ab236e06b11353e3bfbb9fa0c0
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2021-06-22 18:21:23 -0300

    Salvando usuario no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,38 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import { query as q } from 'faunadb';
+
+import NextAuth from 'next-auth'; 
+import Providers from 'next-auth/providers';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user',
     }),
-    // ...add more providers here
   ],
-  // A database is optional, but required to persist accounts in a database
-  // database: process.env.DATABASE_URL,
+  jwt: {
+    secret: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+      
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email }}
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+
+    },
+  }
 })
